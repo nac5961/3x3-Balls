5874a62083f8fd1dbd1ec6e54b95c64211abef41
Nicholas Cato <nac5961@g.rit.edu>
2019-09-20 11:52:32 -0400

    Added a new script for the camera movement.

---
+++ 3X3 Balls/Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    private Transform target;
+
+    public Transform Target
+    {
+        set { target = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void RotateAroundTarget()
+    {
+
+    }
+
+    private void FollowTarget()
+    {
+
+    }
+}
