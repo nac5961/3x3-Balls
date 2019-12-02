cc03dd842f471c4cba31b57d014b162fd6d2b9c9
Nicholas Cato <nac5961@g.rit.edu>
2019-12-01 21:11:02 -0500

    Added a free roam script. Only used for the purpose of making a trailer.

---
+++ 3X3 Balls/Assets/Scripts/Base Mechanics/Camera/FreeRoam.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeRoam : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float rotSpeed = 60.0f;
+    private bool enteredFreeRoam = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.V))
+        {
+            enteredFreeRoam = !enteredFreeRoam;
+
+            if (enteredFreeRoam)
+            {
+                Camera.main.GetComponent<ThirdPersonCamera>().enabled = false;
+                SceneInfo.instance.ActiveBall.GetComponent<PreviewLine>().enabled = false;
+                SceneInfo.instance.Cue.SetActive(false);
+            }
+            else
+            {
+                Camera.main.GetComponent<ThirdPersonCamera>().enabled = true;
+                SceneInfo.instance.ActiveBall.GetComponent<PreviewLine>().enabled = true;
+                SceneInfo.instance.Cue.SetActive(true);
+            }
+        }
+
+        if (enteredFreeRoam)
+        {
+            Move();
+            Rotate();
+        }
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Camera.main.transform.position += Camera.main.transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Camera.main.transform.position += -Camera.main.transform.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Camera.main.transform.position += -Camera.main.transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Camera.main.transform.position += Camera.main.transform.right * speed * Time.deltaTime;
+        }
+    }
+
+    private void Rotate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Camera.main.transform.RotateAround(Camera.main.transform.position, Vector3.up, -rotSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Camera.main.transform.RotateAround(Camera.main.transform.position, Vector3.up, rotSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Camera.main.transform.RotateAround(Camera.main.transform.position, Camera.main.transform.right, -rotSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Camera.main.transform.RotateAround(Camera.main.transform.position, Camera.main.transform.right, rotSpeed * Time.deltaTime);
+        }
+    }
+}
